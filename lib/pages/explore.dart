0f0f4d8d2ba0ef5b6def143d72b459a5a41c4457
KarimAmr-CSCE <81714812+KarimAmr-CSCE@users.noreply.github.com>
2022-09-18 20:42:20 +0200

    Update explore.dart
    Line 6 to 27 is the piece of code to have icons under the search bar, but I need to know what will be the list of places for dog owners would want to find. I have Vets, Parks, and Pet Stores

--- lib/pages/explore.dart
+++ lib/pages/explore.dart
@@ -3,6 +3,28 @@ import 'package:flutter_app_test1/FETCH_wdgts.dart';
 import 'package:flutter_app_test1/routesGenerator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+class Category {
+  String name;
+  Category({@required this.name});
+}
+List<Category> categories = [
+  Category(name: 'Parks'),
+  Category(name: 'Vets'),
+  Category(name: 'Pet Stores'),
+];
+class CategoryRenderingService {
+  List<Category> categories;
+  int selectedIndex = 0;
+CategoryRenderingService({@required this.categories});
+List<Widget> render() {
+    return categories.map((category) {
+      bool selected = categories.indexOf(category) == selectedIndex;
+      
+      TextStyle style = selected ? TextStyle(fontWeight: FontWeight.bold) : TextStyle(fontWeight: FontWeight.normal);
+      return Text(category.name, style: style);
+    }).toList();
+  }
+}
 
 int _selectedIndex = 0;
 class MapsPage extends StatefulWidget {
